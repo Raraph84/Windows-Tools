6e27e7b4003d52f42b767ffae8a7531168a5e1ce
Raraph84 <contact@raraph.fr>
2024-03-27 14:44:01 +0100

    Add setresolution and setrefreshrate commands ; Add refresh rate in list command

--- screens.cpp
+++ screens.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include <windows.h>
 #include <winuser.h>
+#include <vector>
+
+std::vector<MONITORINFOEX> monitors() {
+    std::vector<MONITORINFOEX> monitors;
+    EnumDisplayMonitors(nullptr, nullptr, [](HMONITOR hMonitor, HDC hdcMonitor, LPRECT lprcMonitor, LPARAM dwData) -> BOOL {
+        auto *monitors = reinterpret_cast<std::vector<MONITORINFOEX> *>(dwData);
+        MONITORINFOEX info;
+        info.cbSize = sizeof(MONITORINFOEX);
+        if (GetMonitorInfo(hMonitor, &info))
+            monitors->push_back(info);
+        return TRUE;
+    }, reinterpret_cast<LPARAM>(&monitors));
+    return monitors;
+}
 
 int main(int argc, char *argv[]) {
 
@@ -9,7 +23,7 @@ int main(int argc, char *argv[]) {
     else exe = argv[0];
 
     if (argc < 2) {
-        std::cout << "Usage: " << exe << " <list> [args]" << std::endl;
+        std::cout << "Usage: " << exe << " <list, setresolution, setrefreshrate> [args]" << std::endl;
         return 1;
     }
 
@@ -25,15 +39,82 @@ int main(int argc, char *argv[]) {
         for (int i = 0; i < GetSystemMetrics(SM_CMONITORS); i++) {
             std::cout << "Screen " << i << std::endl;
             std::cout << "Resolution: " << GetSystemMetrics(SM_CXSCREEN) << "x" << GetSystemMetrics(SM_CYSCREEN) << std::endl;
-            // TODO print hz / zoom and orientation
+            std::cout << "Refresh rate: " << GetDeviceCaps(GetDC(nullptr), VREFRESH) << std::endl;
+            // TODO print zoom and orientation
+        }
+
+    } else if (strcasecmp(argv[1], "setresolution") == 0) {
+
+        if (argc != 5) {
+            std::cout << "Usage: " << exe << " " << argv[1] << " <screen> <width> <height>" << std::endl;
+            return 1;
+        }
+
+        char *end;
+        int screen = strtol(argv[2], &end, 0);
+        int width = strtol(argv[3], &end, 0);
+        int height = strtol(argv[4], &end, 0);
+
+        if (screen < 0 || screen >= GetSystemMetrics(SM_CMONITORS)) {
+            std::cout << "This screen does not exist" << std::endl;
+            return 1;
+        }
+
+        if (width < 0 || height < 0) {
+            std::cout << "Invalid resolution" << std::endl;
+            return 1;
+        }
+
+        DEVMODE desiredMode = {0};
+        desiredMode.dmSize = sizeof(DEVMODE);
+        desiredMode.dmPelsWidth = width;
+        desiredMode.dmPelsHeight = height;
+        desiredMode.dmFields = DM_PELSHEIGHT | DM_PELSWIDTH;
+
+        LONG res = ChangeDisplaySettingsEx(monitors()[screen].szDevice, &desiredMode, nullptr, CDS_UPDATEREGISTRY | CDS_GLOBAL | CDS_RESET, nullptr);
+        if (res != DISP_CHANGE_SUCCESSFUL) {
+            std::cout << "Failed to set resolution" << std::endl;
+            return 1;
+        }
+
+    } else if (strcasecmp(argv[1], "setrefreshrate") == 0) {
+
+        if (argc != 4) {
+            std::cout << "Usage: " << exe << " " << argv[1] << " <screen> <refresh rate>" << std::endl;
+            return 1;
+        }
+
+        char *end;
+        int screen = strtol(argv[2], &end, 0);
+        int hz = strtol(argv[3], &end, 0);
+
+        if (screen < 0 || screen >= GetSystemMetrics(SM_CMONITORS)) {
+            std::cout << "This screen does not exist" << std::endl;
+            return 1;
+        }
+
+        if (hz < 0) {
+            std::cout << "Invalid refresh rate" << std::endl;
+            return 1;
+        }
+
+        DEVMODE desiredMode = {0};
+        desiredMode.dmSize = sizeof(DEVMODE);
+        desiredMode.dmDisplayFrequency = hz;
+        desiredMode.dmFields = DM_DISPLAYFREQUENCY;
+
+        LONG res = ChangeDisplaySettingsEx(monitors()[screen].szDevice, &desiredMode, nullptr, CDS_UPDATEREGISTRY | CDS_GLOBAL | CDS_RESET, nullptr);
+        if (res != DISP_CHANGE_SUCCESSFUL) {
+            std::cout << "Failed to set refresh rate" << std::endl;
+            return 1;
         }
 
     } else {
-        std::cout << "Usage: " << exe << " <list> [args]" << std::endl;
+        std::cout << "Usage: " << exe << " <list, setresolution, setrefreshrate> [args]" << std::endl;
         return 1;
     }
 
-    // TODO setmode / setresolution / sethz / setzoom / setorientation
+    // TODO setmode / setzoom / setorientation
 
     return 0;
 }
