5f5d09760d5ecd8771cfdd9a799af9325e39ffcd
Raraph84 <contact@raraph.fr>
2025-02-18 19:53:37 +0100

    Cleanup and fix memory leaks

--- audiousage.cpp
+++ audiousage.cpp
@@ -1,30 +1,15 @@
-#include <windows.h>
 #include <mmdeviceapi.h>
-#include <Audioclient.h>
-#include <endpointvolume.h>
 #include <audiopolicy.h>
 #include <initguid.h> // Required for next include
 #include <functiondiscoverykeys_devpkey.h>
 #include <iostream>
-#include <string>
 #include <thread>
 
-#pragma comment(lib, "ole32.lib")
-
-const CLSID CLSID_MMDeviceEnumerator = __uuidof(MMDeviceEnumerator);
-const IID IID_IMMDeviceEnumerator = __uuidof(IMMDeviceEnumerator);
+HRESULT hr = S_OK;
+IMMDeviceEnumerator* pEnumerator = nullptr;
 
 int get()
 {
-    HRESULT hr = S_OK;
-
-    hr = CoInitialize(nullptr);
-    if (FAILED(hr)) return -1;
-
-    IMMDeviceEnumerator* pEnumerator = nullptr;
-    hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), nullptr, CLSCTX_ALL, __uuidof(IMMDeviceEnumerator), reinterpret_cast<void**>(&pEnumerator));
-    if (FAILED(hr)) return -1;
-
     IMMDeviceCollection* pCollection = nullptr;
     hr = pEnumerator->EnumAudioEndpoints(eCapture, DEVICE_STATE_ACTIVE, &pCollection);
     if (FAILED(hr)) return -1;
@@ -51,16 +36,24 @@ int get()
         PropVariantInit(&varName);
         hr = pProps->GetValue(PKEY_Device_FriendlyName, &varName);
         if (FAILED(hr)) return -1;
+        pProps->Release();
 
         std::wcout << pwszID << " " << varName.pwszVal << std::endl;
+
+        CoTaskMemFree(pwszID);
+        hr = PropVariantClear(&varName);
+        if (FAILED(hr)) return -1;
 
         IAudioSessionManager2* pSessionManager = nullptr;
         hr = pDevice->Activate(__uuidof(IAudioSessionManager2), CLSCTX_ALL, nullptr, reinterpret_cast<void**>(&pSessionManager));
         if (FAILED(hr)) return -1;
 
+        pDevice->Release();
+
         IAudioSessionEnumerator* pSessionEnumerator = nullptr;
         hr = pSessionManager->GetSessionEnumerator(&pSessionEnumerator);
         if (FAILED(hr)) return -1;
+        pSessionManager->Release();
 
         int sessionCount;
         hr = pSessionEnumerator->GetCount(&sessionCount);
@@ -75,15 +68,22 @@ int get()
             IAudioSessionControl2* pSessionControl2 = nullptr;
             hr = pSessionControl->QueryInterface(__uuidof(IAudioSessionControl2), reinterpret_cast<void**>(&pSessionControl2));
             if (FAILED(hr)) return -1;
+            pSessionControl->Release();
 
             AudioSessionState state;
             hr = pSessionControl2->GetState(&state);
             if (FAILED(hr)) return -1;
-            if (state != AudioSessionStateActive) continue;
+            if (state != AudioSessionStateActive)
+            {
+                pSessionControl2->Release();
+                continue;
+            }
 
             DWORD processId;
             hr = pSessionControl2->GetProcessId(&processId);
             if (FAILED(hr)) return -1;
+
+            pSessionControl2->Release();
 
             if (HANDLE hProcess = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, processId); hProcess != nullptr)
             {
@@ -94,26 +94,12 @@ int get()
 
                 CloseHandle(hProcess);
             }
-
-            pSessionControl2->Release();
-            pSessionControl->Release();
         }
 
         pSessionEnumerator->Release();
-        pSessionManager->Release();
-
-        hr = PropVariantClear(&varName);
-        if (FAILED(hr)) return -1;
-
-        if (pProps) pProps->Release();
-        if (pwszID) CoTaskMemFree(pwszID);
-        pDevice->Release();
     }
 
     pCollection->Release();
-    pEnumerator->Release();
-
-    CoUninitialize();
 
     return 0;
 }
@@ -129,6 +115,12 @@ int main(const int argc, char* argv[])
         std::cout << "Usage: " << exe << " <get, pool> [args]" << std::endl;
         return 1;
     }
+
+    hr = CoInitialize(nullptr);
+    if (FAILED(hr)) return -1;
+
+    hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), nullptr, CLSCTX_ALL, __uuidof(IMMDeviceEnumerator), reinterpret_cast<void**>(&pEnumerator));
+    if (FAILED(hr)) return -1;
 
     if (strcasecmp(argv[1], "get") == 0)
     {
@@ -167,6 +159,9 @@ int main(const int argc, char* argv[])
         std::cout << "Usage: " << exe << " <get, pool> [args]" << std::endl;
         return 1;
     }
+
+    pEnumerator->Release();
+    CoUninitialize();
 
     return 0;
 }
